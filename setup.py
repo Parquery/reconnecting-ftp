38e67879bb1cd7f6bd1031bd6e45e61318ae869d
Marko Ristin <marko.ristin@gmail.com>
2022-03-09 08:32:42 +0100

    Added support for Python 3.10
    We include Python 3.10 in the continuous integration to keep up with the
    developments in the Python world.

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     keywords='ftplib reconnect retry robust ftp client',
     packages=find_packages(exclude=['tests*']),
